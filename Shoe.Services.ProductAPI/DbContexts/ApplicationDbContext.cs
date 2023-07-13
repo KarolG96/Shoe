e03e24d47da67a219237213c13f311d796b0430f
KarolG96 <karol.g.96@icloud.com>
2023-07-13 22:23:58 +0200

    13.07_Db

---
+++ Shoe.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shoe.Services.ProductAPI.Models;
+
+namespace Shoe.Services.ProductAPI.DbContexts
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
