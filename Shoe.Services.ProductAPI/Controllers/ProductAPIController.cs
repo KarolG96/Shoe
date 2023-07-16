41fe6c2c3a400c7e5bd0aa8525e19b51cea45a01
KarolG96 <karol.g.96@icloud.com>
2023-07-16 14:06:40 +0200

    Update_16.07

--- Shoe.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Shoe.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -18,7 +18,7 @@ namespace Shoe.Services.ProductAPI.Controllers
 			_productRepository = productRepository;
 			this._response = new ResponseDto();
 		}
-        [Authorize]
+
         [HttpGet]
 		public async Task<object> Get()
 		{
@@ -36,7 +36,6 @@ namespace Shoe.Services.ProductAPI.Controllers
 		}
 
 		[HttpGet]
-        [Authorize]
         [Route("{id}")]
 		public async Task<object> Get(int id)
 		{
--- Shoe.Web/Controllers/HomeController.cs
+++ Shoe.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Shoe.Web.Models;
+using Shoe.Web.Services.IServices;
 using System.Diagnostics;
 
 namespace Shoe.Web.Controllers
@@ -8,15 +10,36 @@ namespace Shoe.Web.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IProductService _productService;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, IProductService productService)
 		{
 			_logger = logger;
+			_productService = productService;
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
-			return View();
+			List<ProductDto> list = new();
+			var response = await _productService.GetAllProductsAsync<ResponseDto>("");
+			if(response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+			}
+
+			return View(list);
+		}
+
+		[Authorize]
+		public async Task<IActionResult> Details(int productId)
+		{
+			ProductDto model = new();
+			var response = await _productService.GetProductByIdAsync<ResponseDto>(productId, "");
+			if (response != null && response.IsSuccess)
+			{
+				model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+			}
+			return View(model);
 		}
 
 		public IActionResult Privacy()
