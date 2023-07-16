ea04fd79e7e7528909d5a8bd4ec8958b966ab7a7
KarolG96 <karol.g.96@icloud.com>
2023-07-16 16:30:59 +0200

    ShoppingCart_done

--- Shoe.Services.ShoppingCartAPI/Controllers/CartController.cs
+++ Shoe.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shoe.Services.ShoppingCartAPI.Models.Dto;
+using Shoe.Services.ShoppingCartAPI.Repository;
 
 namespace Shoe.Services.ShoppingCartAPI.Controllers
 {
@@ -6,9 +8,78 @@ namespace Shoe.Services.ShoppingCartAPI.Controllers
     [Route("api/cart")]
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly ICartRepository _cartRepository;
+        protected ResponseDto _response;
+
+        public CartController(ICartRepository cartRepository)
         {
-            return View();
+            _cartRepository = cartRepository;
+            this._response = new ResponseDto();
         }
+
+        [HttpGet("GetCart/{userId}")]
+        public async Task<object> GetCart(string userId)
+        {
+            try
+            {
+                CartDto cartDto = await _cartRepository.GetCartByUserId(userId);
+                _response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("AddCart")]
+        public async Task<object> AddCart(CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("UpdateCart")]
+        public async Task<object> UpdateCart(CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("RemoveCart")]
+        public async Task<object> RemoveCart([FromBody] int cartId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.RemoveFromCart(cartId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
     }
 }
